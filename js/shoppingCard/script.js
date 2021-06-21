43618a7cac8e7e69dd8806e7e39ef2fc03eb6935
valdiks1 <vlad.pegushyn1@gmail.com>
2021-06-21 17:10:53 +0300

    basket

--- js/shoppingCard/script.js
+++ js/shoppingCard/script.js
@@ -1,57 +1,66 @@
-const products1 = [
-  {
-      id: 0,
-      title: 'Процессор',
-      icon: '1.png',
-      country: 'CША',
-      quantity: 20,
-      price: 10000,
-  },
-  {
-      id: 1,
-      title: 'Клавиатура',
-      icon: '2.png',
-      country: 'CША',
-      quantity: 10,
-      price: 500,
-  },
-  {
-      id: 2,
-      title: 'Корпус',
-      icon: '3.png',
-      country: 'CША',
-      quantity: 20,
-      price: 1000,
-  },]
+function buyBasket(){
+    let inputs = document.querySelectorAll('input');
 
-localStorage.setItem('Basket', JSON.stringify(products1));
+    let productsData = JSON.parse(localStorage.getItem('productsData'));
+
+    for(let i=0;i<productsData.length;i++){
+        // if(productsData[i].quantity <= 0){
+        //     let tr = document.querySelectorAll('.tr');
+        //     tr[i].remove();
+        // }else{
+        //     productsData[i].quantity -= +inputs[i].value;
+        //     localStorage.setItem('productsData', JSON.stringify(productsData));
+        // }
+        //try{
+            productsData[i].quantity -= +inputs[i].value;
+            localStorage.setItem('productsData', JSON.stringify(productsData));
+        // }catch{
+        //     let elTotal = document.querySelector('.totalPrice');
+        //     elTotal.remove();
+        // }
+        if(productsData[i].quantity <= 0){
+            let tr = document.querySelectorAll('.tr');
+            tr[i].remove();
+            let totalHtml = document.querySelector('.totalHtml');
+            totalHtml.innerHTML=0;
+        }
+    }
+}
 
 function outputBasket(){
-  let data = localStorage.getItem('Basket');
-  let basket = JSON.parse(data);
-  let table = document.querySelector('.basket-table');
-  console.log(basket);
-  let el;
-  let totalpricenum = 0;
-  for(let i=0; i<basket.length;i++){
-      el = document.createElement('tr');
-      el.innerHTML=`<td>${i}</td><td>${basket[i].title} (${basket[i].country})</td><td>${basket[i].quantity}</td><td>${basket[i].price}</td>`
-      table.append(el);
-      localStorage.setItem('totalprice', totalpricenum);
-      totalpricenum = +localStorage.getItem('totalprice') + basket[i].price;
-      localStorage.setItem('totalprice', totalpricenum);
-      let num1 = i;
-      let num2 = basket.length;
-      num1++;
+    let data = localStorage.getItem('Basket');
+    let basket = JSON.parse(data);
+    let table = document.querySelector('.basket-table');
+    let basketContainer = document.querySelector('.basket');
+    console.log(basket);
+    let el;
+    let totalpricenum = 0;
+    for(let i=0; i<basket.length;i++){
+        el = document.createElement('tr');
+        el.classList.add(`tr`);
+        el.innerHTML=`<td>${i}</td><td>${basket[i].title} (${basket[i].country})</td><td><input name="${basket[i].title}" value="${basket[i].quantity}"></td><td>${basket[i].price}</td>`
+        table.append(el);
+        localStorage.setItem('totalprice', totalpricenum);
+        totalpricenum = +localStorage.getItem('totalprice') + +basket[i].price;
+        localStorage.setItem('totalprice', totalpricenum);
+        let num1 = i;
+        let num2 = basket.length;
+        num1++;
 
-      //console.log(num1,num2);
-      if(num1 == num2){
-          let totalprice = document.createElement('tr');
-          totalprice.innerHTML=`<td colspan=3>Total:</td><td>${totalpricenum}<td>`;
-          table.append(totalprice);
-      }
-      
-  }
+        //console.log(num1,num2);
+        if(num1 == num2){
+            let totalprice = document.createElement('tr');
+            //totalprice.classList.add('totalPrice');
+            totalprice.innerHTML=`<td colspan=3>Total:</td><td class="totalHtml">${totalpricenum}</td>`;
+            table.append(totalprice);
+            let btnBuy = document.createElement('button');
+            btnBuy.innerHTML='Buy';
+            basketContainer.append(btnBuy);
+
+            btnBuy.addEventListener('click', buyBasket);
+        }
+        
+    }
 }
 
 outputBasket();
